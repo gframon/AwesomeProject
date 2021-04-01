f332f896da8be82146f830306c21785ff9a6a3b2
Ramon Gomez <ramon.gomez@noaa.gov>
2021-04-01 10:26:00 -0400

    Implemented Speakers component.

--- src/containers/Speakers.js
+++ src/containers/Speakers.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import {Text, FlatList, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, FlatList, View, TextInput, Pressable, Image} from 'react-native';
 import {speakers} from '../data/speakers.json';
 import styles from '../containers/styles/sharedStyles';
 import {Footer} from '../components/Footer';
 import {Header} from '../components/Header';
 
 function Speakers() {
+  const [filteredSpeakers, setFilteredSpeakers] = useState(speakers);
+
+  const getSearchText = (text) => {
+    let filteredSpeakersList = speakers.filter((value) =>
+      value.sessions[0].name.toLowerCase().includes(text.toLowerCase()),
+    );
+    setFilteredSpeakers(filteredSpeakersList);
+  };
   return (
     <View>
+      <SearchSessions getSearchText={getSearchText} />
       <FlatList
-        data={speakers}
+        keyboardDismissMode={'on-drag'}
+        keyboardShouldPersistTaps={'always'}
+        data={filteredSpeakers}
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
-        ItemSeparatorComponent={SeparatorComponet}
+        ItemSeparatorComponent={SeparatorComponent}
         pagingEnabled={false}
         ListHeaderComponent={HeaderComponent}
         ListFooterComponent={FooterComponent}
@@ -21,24 +32,67 @@ function Speakers() {
   );
 }
 
-const renderItem = ({item, index}) => {
+const SearchSessions = (props) => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = (text) => {
+    setSearchText(text);
+    props.getSearchText(text);
+  };
+
+  const clearSearch = () => {
+    setSearchText('');
+    props.getSearchText('');
+  };
+
   return (
-    <View>
-      <SpeakersList id={index} name={item.name} bio={item.bio} />
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        value={searchText}
+        onChangeText={(text) => handleSearch(text)}
+        placeholder={'Search Sessions'}
+        returnKeyType={'go'}
+        autoCorrect={false}
+        autoFocus={false}
+        keyboardType={'default'}
+        multiline={false}
+        selectionColor={'green'}
+      />
+      <Pressable onPress={clearSearch} style={styles.clearContainer}>
+        <Image
+          style={styles.clearImage}
+          source={require('../images/multiply-1_Orange.png')}
+        />
+      </Pressable>
     </View>
   );
 };
 
-const SpeakersList = ({id, name, bio}) => {
+const renderItem = ({item, index}) => {
+  return (
+    <View>
+      <SpeakersList
+        id={index}
+        name={item.name}
+        bio={item.bio}
+        session={item.sessions[0].name}
+      />
+    </View>
+  );
+};
+
+const SpeakersList = ({id, name, bio, session}) => {
   return (
     <View style={styles.sectionContainer} key={id}>
       <Text style={styles.sectionTitle}>{`Speakers name: ${name}`}</Text>
       <Text style={styles.sectionDescription}>{`Bio: ${bio}`}</Text>
+      <Text style={styles.sectionDescription}>{`Session: ${session}`}</Text>
     </View>
   );
 };
 
-const SeparatorComponet = () => {
+const SeparatorComponent = () => {
   return <View style={styles.separatorStyle} />;
 };
 
